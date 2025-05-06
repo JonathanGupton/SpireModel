c2c450b6fa5cd67c3c7d5c5d0adbb33bce945d5d
Jonathan Gupton <14988577+JonathanGupton@users.noreply.github.com>
2025-05-06 15:53:29 -0500

    feat: add tokenize_card, parse_card_choices functions

--- SpireModel/logreader.py
+++ SpireModel/logreader.py
@@ -1,7 +1,7 @@
-from functools import cache
 from typing import Generator
 
 from SpireModel.components import acquire
+from SpireModel.components import skip
 from SpireModel.components import CHARACTERS
 
 
@@ -84,3 +84,29 @@ def get_neow_cost(data):
     if neow_cost := data.get("neow_cost", ""):
         return "NEOW COST", neow_cost
     return ()
+
+
+def tokenize_card(card: str) -> tuple[str, ...]:
+    if "+" in card:
+        card, level = card.split("+")
+        return card, *tokenize_number(level)
+    return (card,)
+
+
+def parse_card_choices(card_choices: list[dict]) -> dict[int, tuple[str, ...]]:
+    card_choices_by_floor: dict[int, tuple[str, ...]] = {}
+    for choices in card_choices:
+        floor = choices["floor"]
+        picked = ()
+
+        if "picked" in choices:
+            tokens = tokenize_card(choices["picked"])
+            picked = (acquire(tokens[0]), *tokens[1:])
+
+        not_picked_cards = []
+        for card in choices["not_picked"]:
+            tokens = tokenize_card(card)
+            not_picked_cards.append(skip(tokens[0]))
+            not_picked_cards.extend(tokens[1:])
+        card_choices_by_floor[floor] = (*picked, *not_picked_cards)
+    return card_choices_by_floor
